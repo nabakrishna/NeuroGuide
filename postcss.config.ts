0cf53b68919ecb561bf612dd99e14898c4ceba5f
nabakrishna <nabakrishna0111@gmail.com>
2026-02-03 18:35:04 +0530

    update the postcss.config.ts

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,6 +1,19 @@
-export default {
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+
+// iam chning the file from postcss.config.ts to postcss.config.mjs
+
+//new code compatabi;ty with vercel ts-node
+
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+export default config;
